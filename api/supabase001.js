ec67f2b39b2fe3233b4f5a064da5a9450c2ee751
tintin94230 <blondel.mail@gmail.com>
2026-02-07 15:07:40 +0100

    aaa

--- api/supabase001.js
+++ api/supabase001.js
@@ -1,12 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("💡 DEBUG ENV - SUPABASE_URL =", process.env.SUPABASE_URL);
-console.log("💡 DEBUG ENV - SUPABASE_KEY =", process.env.SUPABASE_KEY?.slice(0, 4) + "…"); // on masque le reste
-
-console.log("API appelée");
-console.log("process.env.SUPABASE_KEY:", process.env.SUPABASE_KEY?.slice(0,5));
-
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
@@ -38,7 +31,6 @@ export default async function handler(req, res) {
     }
 
     return res.status(405).json({ error: "Méthode non autorisée" });
-
   } catch (err) {
     return res.status(500).json({ error: "Erreur serveur", details: err.message });
   }
